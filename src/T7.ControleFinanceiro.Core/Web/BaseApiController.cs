db76154d6ef4a1fbdecd5478c046e24aea311352
t7tech <julio.tamogami@outlook.com>
2017-04-25 12:46:10 -0300

    base webapi controller

---
+++ src/T7.ControleFinanceiro.Core/Web/BaseApiController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+using Microsoft.AspNet.Identity;
+
+namespace T7.ControleFinanceiro.Core.Web
+{
+    public abstract class BaseApiController : ApiController
+    {
+        #region Attributes
+
+        protected string UserId
+        {
+            get
+            {
+                return User.Identity.GetUserId();
+            }
+        }
+
+        #endregion
+    }
+}
---
+++ src/T7.ControleFinanceiro.UI/Areas/Api/Controllers/ProfileController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Web.Http;
+using T7.ControleFinanceiro.Core.Web;
+
+namespace T7.ControleFinanceiro.UI.Areas.Api.Controllers
+{
+    public class ProfileController : BaseApiController
+    {
+        // GET api/<controller>
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2", UserId };
+        }
+
+        // GET api/<controller>/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<controller>
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
